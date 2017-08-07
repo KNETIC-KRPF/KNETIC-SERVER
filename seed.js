e5e7dd9c174d1a2bcc848ef8ba3bb029dcced13d
Alex Read <alexread730@gmail.com>
2017-08-07 16:16:48 -0600

    seed data working

--- seed.js
+++ seed.js
@@ -1,8 +1,16 @@
 const db = require('monk')('localhost:27017/patchdb');
 const patch = require('./patch');
+const account = require('./account-samples.js')
 const patches = db.get('patches');
+const accounts = db.get('accounts');
 
 patches.remove();
+accounts.remove();
+
+accounts.insert(account).then((result) => {
+    console.log(result);
+    db.close();
+});
 
 patches.insert(patch).then((result) => {
     console.log(result);
