62c3fa2a955cbea5f64c0d762494cbc0b414672d
Alex Read <alexread730@gmail.com>
2017-08-07 15:34:44 -0600

    working account routes

---
+++ routes/account.js
@@ -0,0 +1,39 @@
+const mongo = require('mongodb');
+const express = require('express');
+const router = express.Router();
+const monk = require('monk');
+const db =  monk('localhost:27017/patchdb');
+const accounts = db.get('accounts');
+
+
+router.get('/',(req,res) => {
+  const data = accounts.find({});
+  return data.then((data) => {
+    res.json(data)
+  });
+});
+
+router.post('/', (req, res, next) => {
+  accounts.insert(req.body)
+    .then(response => {
+      res.json(response);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  accounts.findOneAndUpdate({_id:req.params.id}, req.body)
+    .then(response => {
+      res.json(response);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  accounts.findOneAndDelete({_id:req.params.id})
+    .then(response => {
+      res.json(response);
+    });
+});
+
+
+
+module.exports = router;
