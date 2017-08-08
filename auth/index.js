a0dccbbd36ad059ce29cfbc0f3e78bd75bbe53b6
Ross Kiser <kiserr10@gmail.com>
2017-08-08 11:21:22 -0600

    login route working

--- auth/index.js
+++ auth/index.js
@@ -20,9 +20,7 @@ router.post('/signup', (req, res, next) =>{
 			if(account) {
 				next(new Error('Email Already In Use!'));
 			} else {
-				const account = {
-					email: req.body
-				};
+				const account = req.body;
 				bcrypt.hash(req.body.password, 8)
 				.then((hash) =>{
 					account.password = hash;
@@ -36,6 +34,33 @@ router.post('/signup', (req, res, next) =>{
 		});
 	} else {
 		next(new Error ('Invalid User Info!'));
+	}
+});
+
+router.post('/login', (req, res, next) =>{
+	if(validUser(req.body)){
+		console.log(req.body.email);
+		accounts.findOne( { email: req.body.email } )
+		.then((account) =>{
+			console.log(account);
+			if(account) {
+				bcrypt.compare(req.body.password, account.password)
+				.then((result) =>{
+					if(result){
+						res.json({
+							account,
+							message: 'You Are Successfully Logged In!'
+						});
+					} else {
+						next(new Error ('Incorrect Password!'));
+					}
+				});
+			} else {
+				next(new Error('Email Does Not Exist, Please Sign Up With A New Account'));
+			}
+		});
+	} else {
+		next(new Error('The Information You Entered Is Not Valid'));
 	}
 });
 
