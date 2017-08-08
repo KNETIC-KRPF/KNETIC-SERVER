1177b9bd2c685d2c0a04b90e99edf36e26615b84
Ross Kiser <kiserr10@gmail.com>
2017-08-08 12:17:58 -0600

    jwt working on the signup route

--- auth/index.js
+++ auth/index.js
@@ -5,6 +5,11 @@ const mongo = require('mongodb');
 const monk = require('monk');
 const db =  monk('localhost:27017/patchdb');
 const accounts = db.get('accounts');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+
 
 function validUser(account){
 	const validEmail = typeof account.email == 'string' && account.email.trim() != '';
@@ -12,28 +17,42 @@ function validUser(account){
 	return validEmail && validPassword;
 }
 
-router.post('/signup', (req, res, next) =>{
-	if(validUser(req.body)){
-		accounts.findOne( { email: req.body.email } )
-		.then((account) =>{
-			if(account) {
-				next(new Error('Email Already In Use!'));
-			} else {
-				const account = req.body;
-				bcrypt.hash(req.body.password, 8)
-				.then((hash) =>{
-					account.password = hash;
-					accounts.insert(account)
-					.then(account =>{
-						res.json(account);
-					});
 
-				});
-			}
-		});
-	} else {
-		next(new Error ('Invalid User Info!'));
-	}
+router.post('/signup', (req, res, next) =>{
+    if(validUser(req.body)){
+        accounts.findOne( { email: req.body.email } )
+        .then((account) =>{
+            if(account) {
+                next(new Error('Email Already In Use!'));
+            } else {
+                const account = req.body;
+                bcrypt.hash(req.body.password, 8)
+                .then((hash) =>{
+                    account.password = hash;
+                    accounts.insert(account)
+                    .then(account =>{
+                        jwt.sign({
+                            id: account._id,
+                        }, process.env.TOKEN_SECRET, {
+                            expiresIn: '7d'
+                        }, (err, token) =>{
+                            console.log('err', err);
+                            console.log('token', token);
+                            res.json({
+                                id: account._id,
+                                email: account.email,
+                                token: token,
+                                message: 'New Account Created'
+                            })
+                        })
+                    });
+
+                });
+            }
+        });
+    } else {
+        next(new Error ('Invalid User Info!'));
+    }
 });
 
 router.post('/login', (req, res, next) =>{
